a3dbf4564e61af28d9942ba8392c989b1322f5b8
yutakaki <yu.takaki2000@gmail.com>
2022-05-15 11:07:39 +0900

    nav in desktp view finish

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react'
 
 interface LayoutProps {
@@ -17,18 +19,41 @@ const Layout = ({children} : LayoutProps) => {
   ]
 
   return (
-    <div className='flex h-screen text-gray-600'>
-      <nav className='w-1/4 max-w-xs' >
-        <ul>
+    <div className='min-h-screen flex text-slate-400'>
+      <nav className='w-1/4 max-w-xs hidden md:block bg-slate-800' >
+        <figure className='flex justify-center m-3'>
+          <Image
+            src="/images/sample.jpg"
+            className='rounded-full object-cover'
+            alt="user"
+            width={100}
+            height={100}
+          />
+        </figure>
+        <div className='flex flex-col'>
+          <h1 className='text-center text-3xl mb-2'>Hello Yu</h1>
+          <button className='bg-yellow-100 p-2 rounded-md w-3/4 m-auto text-slate-800 font-semibold'>Upload</button>
+        </div>
+        <ul className='mt-3'>
           {nav_options.map(_option => (
-            <li
-              className='text-2xl'
-              key={_option
-            }>{_option}</li>
+            <Link href={`/${_option}`} key={_option}>
+              <a>
+                <li
+                  className='text-2xl'
+                >
+                  <button 
+                    className='hover:bg-slate-400 hover:text-slate-800 w-full text-left p-2'
+                  >
+                    {_option}
+                  </button>
+                </li>
+              </a>
+            </Link>
+            
           ))}
         </ul>
       </nav>
-      <main className='w-full'>
+      <main className='w-full h-screen overflow-auto p-2'>
         {children}
       </main>
     </div>
