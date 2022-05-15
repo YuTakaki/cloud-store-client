a70b2e3244b4cbf1990c06091fae36e80ffa146e
yutakaki <yu.takaki2000@gmail.com>
2022-05-15 14:36:01 +0900

    menu toggle in basic sort

--- components/dashboard/BasicFilter.tsx
+++ components/dashboard/BasicFilter.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import {
   MdArrowUpward
 } from 'react-icons/md'
 
 const BasicFilter = () => {
+  const menuRef = useRef<HTMLMenuElement>(null)
   return (
     <div id="basic-filter" className="flex mt-12 mx-4 justify-end">
       <div className='flex gap-5 items-center'>
         <div className='relative'>
-          <button className=' hover:bg-gray-200 px-4 py-2 rounded font-semibold text-lg'>Date</button>
-          <menu className='absolute z-10 bg-white card w-28 mt-2 p-2 -left-10'>
+          <button
+            className=' hover:bg-gray-200 px-4 py-2 rounded font-semibold text-lg'
+            onMouseDown={() => menuRef.current?.classList.add("hidden")}
+            onMouseUp={() => menuRef.current?.classList.remove("hidden")}
+          >Date</button>
+          <menu
+            ref={menuRef}
+            className='absolute z-10 bg-white card w-28 mt-2 p-2 -left-10 hidden duration-500'
+          >
             <ul>
               <li>sdsd</li>
               <li>sdsd</li>
