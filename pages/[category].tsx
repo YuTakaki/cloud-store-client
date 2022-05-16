163e78b3bdc599466e59494979a1f670f660af4e
yutakaki <yu.takaki2000@gmail.com>
2022-05-16 21:40:21 +0900

    add axios config

--- pages/[category].tsx
+++ pages/[category].tsx
@@ -7,9 +7,6 @@ import BasicSort from '../components/dashboard/BasicSort';
 const Category: NextPage = () => {
 
   const router = useRouter();
-  const { category } = router.query;
-  console.log(category);
-
   return (
     <Layout>
       <BasicSort />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,38 @@
-import * as React from 'react';
+import { GetServerSideProps, NextPageContext } from 'next';
 import { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import "../styles/globals.css"
+import { get } from '../utils/requests';
 
 function MyApp({ Component, pageProps } : AppProps) {
+  useEffect(() => {
+    console.log('asdasd');
+
+  }, []);
   return (
       <Component {...pageProps} />
   )
+}
+
+MyApp.getInitialProps = async({ctx}: any) => {
+  const props = {};
+  try {
+    console.log(ctx.req)
+    const verifyToken = await get('/api/auth/verify', {
+      header : {
+        Cookie: ctx.req!.headers.cookie!
+      }
+    });
+
+    // console.log(verifyToken);
+    
+  } catch (error) {
+    // console.log(error, 'yutakaki'); 
+  }
+  return {
+    props
+  }
+
 }
 
 export default MyApp
